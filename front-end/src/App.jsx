76b7185475f2eebe77ff94ccf68813ee2b880e23
Ishan Garg <ishan.garg10394@gmail.com>
2024-11-16 19:07:07 -0500

    Added Navigation

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,14 +2,24 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CustomNavbar from './components/CustomNavbar';
 import Home from './pages/Home'
+import FindRoommates from './pages/FindRoommates';
+import Events from './pages/Events';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+      <Router>
       <CustomNavbar />
-      <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/findroommates" element={<FindRoommates />} />
+        </Routes>
+      </Router>
+      {/* <Home /> */}
     </>
   )
 }
---
+++ front-end/src/pages/FindRoommates.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const FindRoommates = () => {
+
+}
+
+export default FindRoommates
