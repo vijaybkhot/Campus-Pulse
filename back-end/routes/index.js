27b6624f4af3501ed64327170bd832b59df2b15f
Vijay Khot <vijaysinh.khot@gmail.com>
2024-11-21 21:16:18 -0500

    Merged Backend

--- back-end/routes/index.js
+++
@@ -1,6 +0,0 @@
-import eventRouter from "./eventRoutes.js";
-import userRouter from "./userRoutes.js";
-import roommateRouter from "./roommateRoutes.js";
-import newsFeedRouter from "./newsFeedRoutes.js";
-
-export { eventRouter, userRouter, roommateRouter, newsFeedRouter };
--- back-end/utils/middlewares/loggingMiddleware.js
+++
@@ -1,7 +0,0 @@
-import morgan from "morgan";
-
-export default (app) => {
-  if (process.env.NODE_ENV === "development") {
-    app.use(morgan("dev"));
-  }
-};
